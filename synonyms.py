8c5915be29fe45081ba59ac27652c7eb358540a6
vogeldylan <Dylan@Ikiru.local>
2016-11-29 18:53:37 -0500

    Finished synonyms.py, added test code, prepared report for submission

--- synonyms.py
+++ synonyms.py
@@ -1,15 +1,14 @@
-'''Semantic Similarity: starter code
+'''Semantic Similarity
 
-Author: Michael Guerzhoy. Last modified: Nov. 14, 2016.
+Authors: Danial Hasan, Dylan Vogel, and Michael Guerzhoy. Last modified: Nov. 29, 2016.
 '''
 
-import math, time
+import math
 
 
 def norm(vec):
-    '''Return the norm of a vector stored as a dictionary,
-    as described in the handout for Project 3.
-    '''
+    ''' Return a float of the norm of a vector vec stored as a dictionary.
+        Assume all dictionary values are ints'''
     
     sum_of_squares = 0.0  
     for x in vec:
@@ -17,25 +16,55 @@ def norm(vec):
     
     return math.sqrt(sum_of_squares)
 
-
 def cosine_similarity(vec1, vec2):
-    ''' Return the cosine similaryt of two vectors stored as dictionaries
-        Assume all dictionary values are integers'''
+    ''' Return a float of the cosine similarity of two vectors vec1, vec2, stored as        
+        dictionaries. 
+        Assume all dictionary values are ints, and that neither vector is empty.'''
         
     dot_prod = 0
     for a in vec1:
         if a in vec2:
             dot_prod += vec1[a] * vec2[a]
     magnitude_prod = norm(vec1) * norm(vec2)
-    
-    if dot_prod == 0:
-        return -1
         
     return dot_prod/magnitude_prod
+    
+def euclidian_similarity(vec1, vec2):
+    ''' Return a float of the negative distance in Euclidian space between two vectors 
+        vec1, vec2, stored as dictionaries.
+        Assume all dictionary values are ints'''
+    
+    combined_vec = {**vec1, **vec2}
+    vector_sub = {}
+    
+    for word in combined_vec:
+        vector_sub[word] = vec1.get(word, 0) - vec2.get(word, 0)
+    
+    return norm(vector_sub) * -1
 
+def euclidian_similarity_norm(vec1, vec2):
+    ''' Return a float of the negative distance in Euclidian space between two normalized 
+        vectors vec1, vec2, stored as dictionaries.
+        Assume all dictionary values are ints, and that neithe vector is empty'''
+    
+    vector_sub = {}
+    
+    norm1 = norm(vec1)
+    norm2 = norm(vec2)
+    
+    combined_vec = {**vec1, **vec2}
+    
+    for word in combined_vec:
+        vector_sub[word] = (vec1.get(word, 0) / norm1) - (vec2.get(word, 0) / norm2)
+        
+    return norm(vector_sub) * -1
+            
 
 def build_semantic_descriptors(sentences):
-    #Convential method; test against (try and except) to compare run times
+    ''' Return a dictionary whose values are a dictionary of the number of times a word
+        appears alongside the key word across all sentences in sentences.
+        Assume sentences is a list of a list of strings '''
+        
     d = {}
     for sentence in sentences:
         words_in_sent = []
@@ -51,28 +80,13 @@ def build_semantic_descriptors(sentences):
                         d[key_word][accomp_word] = 0
                     d[key_word][accomp_word] += 1
     return d
-    
-
-
-
-# def build_semantic_descriptors_from_files(filenames):
-#     sentences = []
-#     start = time.time()
-#     for n in range(len(filenames)):
-#         file = open(filenames[n], encoding="latin1").read().lower()
-#         file = file.replace('!', " ").replace('.', ' ').replace('?', ' ')
-#         file = file.split()
-#         sentences.append(file)
-#     #print(sentences)
-#     dictionary = build_semantic_descriptors(sentences)
-#     #print(dictionary['pride'])
-#     print(time.time() - start)
-#     #return dictionary
-
 
 def build_semantic_descriptors_from_files(filenames):
+    ''' Return a dictionary whose values are a dictionary of the number of times a word
+        appears alongside the key word across all sentences in the texts in filenames.
+        Assume filenames is a valid list of text files stored as strings.'''
+    
     sentences = []
-    start = time.time()
     for text in filenames:
         file = open(text, encoding="utf8").read().lower().replace("\n", " ").replace(',', ' ').replace('-',' ').replace('--', ' ').replace(':',' ').replace(';',' ').split('.')
         for i in range(len(file)):
@@ -82,12 +96,15 @@ def build_semantic_descriptors_from_files(filenames):
                 for k in range(len(file[i][j])):
                     file[i][j][k] = file[i][j][k].split()
                     sentences.append(file[i][j][k])
-    print("success, building descriptors")
     dictionary = build_semantic_descriptors(sentences)
-    print(time.time() - start)
     return dictionary
 
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
+    ''' Return the string in the list choices that is most semantically similar to
+        word word when tested using the similarity function similarity_fn.
+        Assume word is a string, similarity_fn is a function, choices is a list 
+        of strings and semantic descriptors is a dictionary of dictionaries.'''
+    
     scores = []
     if word not in semantic_descriptors:
         return -1
@@ -100,6 +117,15 @@ def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
     
 
 def run_similarity_test(filename, semantic_descriptors, similarity_fn):
+    ''' Return a float of the percentage of correct answers returned for the 
+        semantic similarity questions in file filename.
+        
+        Assume filename is a valid file name stored as a string containing lines 
+        of words where the first word is the base word to be compared, the second
+        word is the answer, and subsequent words are the available choices.
+        Assume semantic_descriptors is a dictionary of dictionaries, and similarity_fn 
+        is a valid function. '''
+    
     correct = 0
     
     file = open(filename, encoding="latin1").read().split("\n")
@@ -109,18 +135,81 @@ def run_similarity_test(filename, semantic_descriptors, similarity_fn):
             del file[i]
             i -= 1
             continue
-        if file[i][1] == most_similar_word(file[i][0], file[i][2:], semantic_descriptors, cosine_similarity):
+        if file[i][1] == most_similar_word(file[i][0], file[i][2:], semantic_descriptors, similarity_fn):
             correct += 1
     
-    return correct/len(file)
+    return correct/len(file) * 100
+
 
 if __name__ == "__main__":
-    filenames = ["swann.txt", "war_and_peace.txt"]
-    semantic_descriptors = build_semantic_descriptors_from_files(filenames)
-    #print(semantic_descriptors['majestic'])
-    #print(build_semantic_descriptors([['hello', 'hello', 'hello', 'hi', 'hi', 'blue', 'blue'], ['hello', 'hi']]))
+    #Test 1: Test cosine_similarity & norm function
+    print("Test 1")
+    vec1 = {"a":1, "b":2, "c":3}
+    vec2 = {"b":1, "c":2, "d":3}
+    if round(norm(vec1), 2) == 3.74:       # sqrt(1**2 + 2**2 + 3**2) ~ 3.74
+        print("a) Test Passed")
+    else:
+        print("a) Test Failed")
     
-    #choices = ['him', 'her', 'woman', 'prejudice']
-    # print(most_similar_word(man, choices, semantic_descriptors, cosine_similary))
+    if round(cosine_similarity(vec1, vec2), 2) == 0.57:     # 8 / 14 ~ 0.57
+        print("b) Test Passed") 
+    else:
+        print("b) Test Failed")
     
+    #Test 2: Test build_semantic_descriptors
+    print("=======================================", "\nTest 2")
+    sentences = [["alfa", "bravo", "charlie", "delta"], ["alfa", "bravo", "echo", "foxtrot", "alfa"]]
+    test_case = build_semantic_descriptors(sentences)
+    if test_case["alfa"]["bravo"] == 2:    # Alfa should only occur twice between the two sentences, and not be double counted.
+        print("a) Test Passed")
+    else:
+        print("a) Test Failed")
     
+    if test_case["bravo"]["charlie"] == 1: # There should only be one occurence of charlie between both sententces bravo is in
+        print("b) Test Passed")
+    else:
+        print("b) Test Failed")
+    
+    if test_case["delta"].get("foxtrot", None) == None: # Delta and foxtrot never appear together
+        print("c) Test Passed")
+    else:
+        print("c) Test Failed")
+        
+    #Test 3: Test build_semantic_descriptors_from_files
+    print("=======================================", "\nTest 3")
+    # sample.txt is a text file in the root directory where synonyms.py is stored. These tests are commented out to avoid conflicts.
+    # sample.txt:
+    # Alfa; bravo charlie delta.
+    # alfa-bravo, echo? Foxtrot!
+    # Foxtrot: echo--foxtrot.
+    
+    '''test_dic = build_semantic_descriptors_from_files(["sample.txt"])
+    if test_dic["foxtrot"] == {"echo":1}:       # Checks if properly splits sentences based on "!", "?"; Doesn't double count, ignores capitilization. Ignores "--", ":".
+        print("a) Test Passed")
+    else:
+        print("a) Test Failed")
+    if test_dic["alfa"]["bravo"] == 2:          # Checks if properly splits sentences based on "."; Ignores ";", "-", ",".
+        print("b) Test Passed")
+    else:
+        print("b) Test Failed")
+    '''
+    
+    #Test 4: Test most_similar_word
+    print("=======================================", "\nTest 4")
+    # See above comment on sample.txt
+    
+    '''test_dic = build_semantic_descriptors_from_files(["sample.txt"])
+    # Delta-alfa cosine similarity ~ 0.65
+    # Bravo-alfa cosine similarity ~ 0.43
+    if most_similar_word("alfa", ["bravo", "delta"], test_dic, cosine_similarity) == "delta":
+        print("a) Test Passed")
+    else:
+        print("a) Test Failed")
+    # Foxtrot-echo cosine similarity = 0, no common words
+    # Bravo-echo cosine similarity ~ 0.44
+    # Golf-echo cosine similarity = 0, golf not in dictionary
+    if most_similar_word("echo", ["foxtrot", "bravo", "golf"], test_dic, cosine_similarity) == "bravo":
+        print("b) Test Passed")
+    else:
+        print("b) Test Failed")
+    '''
